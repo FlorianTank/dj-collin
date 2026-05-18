50286ed02ad7aa23a056bf06a012f5a574454a36
florian <=>
2026-05-18 09:29:39 +0200

    Add Leistungen

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        ueberMich: resolve(__dirname, "ueber-mich/index.html"),
+        main:       resolve(__dirname, "index.html"),
+        ueberMich:  resolve(__dirname, "ueber-mich/index.html"),
+        leistungen: resolve(__dirname, "leistungen/index.html"),
+        hochzeiten: resolve(__dirname, "leistungen/hochzeiten/index.html"),
       },
     },
   },
